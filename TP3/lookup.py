16d4e5af241831937f3af27bd5bc922ebef5fefe
NepNath <neptonath@gmail.com>
2024-10-11 09:07:23 +0200

    lookup

--- TP3/lookup.py
+++ TP3/lookup.py
@@ -1,10 +1,24 @@
 import socket
 import os
 from sys import argv
+import re
+
+def url_check(url):
+
+    url_regex = re.compile(
+        r'^(https?:\/\/)?'  
+        r'(([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6})'  
+        r'(\/[a-zA-Z0-9_.-]*)*\/?$'  
+    )
+    
+
+    return re.match(url_regex, url) is not None
 
 hstnm = argv [1]
 
+if url_check(hstnm) == True:
+    ipaddr = socket.gethostbyname(hstnm)
+    print(ipaddr)
+else : 
+    print("erreur URL")
 
-ipaddr = socket.gethostbyname(hstnm)
-
-print(ipaddr)
