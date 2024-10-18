df8792c870f9640a0d2e00239b271876a2b5b970
NepNath <neptonath@gmail.com>
2024-10-18 10:21:33 +0200

    test

--- TP4/bs_client_I1.py
+++ TP4/bs_client_I1.py
@@ -8,6 +8,14 @@ port = 13337
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((host, port))
+while True : 
+    try :
+        print(f"Connection à {host} réussie avec le port {port}")
+
+    except socket.error: 
+        print("Sorry, an error Occured")
+        break
+
 
 s.sendall(b'Meooooo !')
 
